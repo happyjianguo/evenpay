f86d697568d5de97010f3e42d89cd9a5d46b93c6
jinyiming <jinym@csxbank.com>
2019-07-31 14:52:22 +0800

    增加github打包支持和ignore文件

--- package.py
+++ package.py
@@ -5,28 +5,50 @@ import sys
 import os
 import re
 
-active = sys.argv[0]
+active = sys.argv[1]
 latest = 'latest'
-region='hongkong'
+region = None
+app = None
+
+
+try:
+    region=sys.argv[2]
+except:
+    region = "shenzhen"
+try:
+    app = sys.argv[3]
+except:
+    pass
+
 
 env = os.environ
-branch = os.getenv('BITBUCKET_BRANCH', 'master')
-commit = os.getenv('BITBUCKET_COMMIT', 'abcd123')
+
+branch = os.getenv('TRAVIS_REPO_SLUG', active)
+commit = os.getenv('TRAVIS_COMMIT', 'abcd123')
 tag = 'latest'
 
 if re.search(r'master|release', branch):
-    tag = 'latest'
-if re.search(r'test|staging', branch):
+    tag = 'prod'
+    region = 'hongkong'
+    active = 'prod'
+elif re.search(r'test|staging', branch):
     tag = branch
-if re.search(r'dev', branch):
+    active = 'test'
+elif re.search(r'dev', branch):
     tag = 'dev'
+    active = 'dev'
+else:
+    tag = active
 
 print(os.environ['HOME'])
-print "active = {0}, region = {1}, tag={2}, commit={3}".format(active, region, tag, commit)
+print "active = {0}, region = {1}, tag={2}, commit={3}".format(active, region, tag, commit))
 
 project = 'xxpay'
 version = '1.0.0'
 app_list = ['service', 'agent', 'manage', 'merchant', 'pay', 'task']
+if app is not None:
+    app_list = app.split(",")
+
 app_opt = {
     'service': { 'port': 8190, 'main': 'org.xxpay.service.XxPayServiceAppliaction' },
     'merchant': { 'port': 8191 },
@@ -37,20 +59,17 @@ app_opt = {
 }
 
 
-for app in app_opt:
+
+# mvn clean build
+os.system("mvn clean install -P" + tag)
+# for app in app_opt:
+for app in app_list:
     app_path = '-'.join([project,app])
     # build docker image and push
     cwd = os.getcwd()
     build_path = os.path.join(cwd, app_path)
     os.chdir(build_path)
     print "build_path = ", build_path
-    #  IMAGE_NAME=haodiaodemingzi/cloudfeet:$BITBUCKET_COMMIT
-    # docker push haodiaodemingzi/evenpay-agent:tagname
-
-    # $ sudo docker login --username=赚钱的大氓 registry.cn-shenzhen.aliyuncs.com
-    # $ sudo docker tag [ImageId] registry.cn-shenzhen.aliyuncs.com/evencc/evenpay:[镜像版本号]
-    # $ sudo docker push registry.cn-shenzhen.aliyuncs.com/evencc/evenpay:[镜像版本号]
-    #image_name = "registry.cn-shenzhen.aliyuncs.com/evencc/evenpay-{0}:latest".format(app)
     for item in (tag, commit):
         image_name = "registry.cn-{0}.aliyuncs.com/evencc/evenpay-{1}:{2}".format(region, app, item)
         cmd1 = "docker build -t {0} .".format(image_name)
@@ -59,7 +78,6 @@ for app in app_opt:
 
         cmd2 = "docker push {0}".format(image_name)
         print cmd2
-        # docker push haodiaodemingzi/evenpay-agent:tagname print cmd2
         os.system(cmd2)
 
     os.chdir(cwd)
